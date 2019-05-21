47be0354ebc52260bed82983335a6df6dbe09c88
shi974 <shidl@rocketmail.com>
2019-05-21 09:16:27 +0400

    update To Do List

--- JavaScript/To Do List/main.js
+++ JavaScript/To Do List/main.js
@@ -23,16 +23,31 @@ var app = new Vue({
             }
         ]
     },
+    mounted() {
+        if (localStorage.getItem('todos')) { // Initie la sauvegarde
+            try {
+                this.todos = JSON.parse(localStorage.getItem('todos'));
+            } catch (g) {
+                localStorage.removeItem('todos');
+            }
+        }
+    },
     methods: {
         addTodo: function (e) { // Ajoute dynamiquement à l'array donc à la liste
             this.todos.push({
-                text: e, completed: false, edit: false
+                text: e,
+                completed: false,
+                edit: false
             });
             this.element = ''; // Clear form après envoi
         },
         removeTodo: function (e) { // Fonction pour enlever un item
             this.todos.splice(this.todos.indexOf(e), 1);
         },
+        saveTodos() { // Sauvegarde dans le JSON - local storage
+            const parsed = JSON.stringify(this.todos);
+            localStorage.setItem('todos', parsed);
+        }
     },
     computed: {
         count() { // Renvoie un compteur de tâches non complétées
